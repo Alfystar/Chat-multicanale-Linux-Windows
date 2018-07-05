7377a944c253f70da7711571a6e4849ce3ce8175
Alfystar <32840211+Alfystar@users.noreply.github.com>
2018-07-05 19:18:56 +0200

    clion start project

---
+++ cmake-build-debug/CMakeFiles/Chat_MultiLevel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat_MultiLevel.dir/main.c.o"
+  "Chat_MultiLevel.pdb"
+  "Chat_MultiLevel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chat_MultiLevel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

275eb8fad8aa44ea40c04114204639edab1e54e5
alfylinux <emanuele.alfano@alice.it>
2018-07-05 20:08:19 +0200

    start

--- main.c
+++ main.c
@@ -5,23 +5,75 @@ int main() {
     return 0;
 }
 
+/***************************************************************/
+/*Funzioni per riconoscere/creare/rimuovere Id utenti messaggio*/
 
-/*************************************************/
-/*Funzioni per riconoscere il pacchetto messaggio*/
+/*
+ * Il file dovra contenere gli id degli utenti e contenere
+ * anche il loro nome equivalente in codice ascii
+ *
+ *
+ * (Opzionale)
+ * Tenere traccia anche di una password per poter accedere.
+ *
+ * /---|----|---|-----------|----|---\
+ * | 1 | Id | 2 |   NAME    |'\0'| 3 |
+ * \---|----|---|-----------|----|---/
+ *
+ * -1: in ascii indica "Start Of Heading"
+ * -2: in ascii indica "Start Of Text"
+ * -3: in ascii indica "End Of Text"
+ *
+ * (Opzionale)
+ * -4: -2: in ascii indica "End of Trasmission"
+ */
+
+
+/***************************************************************/
+/*Funzioni per riconoscere/creare/rimuovere pacchetti messaggio*/
 
 /*
  * La struttura del pacchetto è del tipo:
  *
  * i numeri sono i valori decimali dell'equivalente ascii
  *
- * /---|----|---|-----|-----------|----|---\
- * | 1 | Id | 2 | Ora |   TEXT    |'\0'| 3 |
- * \---|----|---|-----|-----------|----|---/
+ * /---|----|-----|---|-----------|----|---\
+ * | 1 | Id | Ora | 2 |   TEXT    |'\0'| 3 |
+ * \---|----|-----|---|-----------|----|---/
  *
  * -1: in ascii indica "Start Of Heading"
  * -2: in ascii indica "Start Of Text"
  * -3: in ascii indica "End Of Text"
  *
- * (opzionale)
+ * (Opzionale)
  * -4: -2: in ascii indica "End of Trasmission"
+ */
+
+
+/***************************************************************/
+/*Funzioni per creare la struttura delle room nel file-sistem  */
+
+/*
+ * Dir-Server/
+ *  |---Dir-chat-Name 1
+ *  |   |--- ...
+ *  |
+ *  |---Dir-chat-Name 2
+ *  |   |--- ...
+ *  |
+ *  |---Dir-chat-Name 3
+ *  |   |--- ...
+ *  |
+ *  |--- File-User-Login-data
+ *
+ *
+ * Name-File = Name-chat
+ * Storico-chat : Name-chat-firstMessageData
+ *
+ * /"NoDir-chat-Name"\
+ *  |--- File: attuale chat
+ *  |---Dir-history/
+ *  |   |--- File-hystory xxxx-mm-gg
+ *  |   |--- File-hystory xxxx-mm-gg
+ *  |   ...
  */
